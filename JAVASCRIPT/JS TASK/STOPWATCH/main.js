71a52c6b73b7d1a2addbda1ed5a8f001ad6323c5
Mr. Shyam Manvar <142787012+shyammanvar@users.noreply.github.com>
2023-10-06 16:06:06 +0530

    Add files via upload

---
+++ JAVASCRIPT/JS TASK/STOPWATCH/main.js
@@ -0,0 +1,11 @@
+const timerEl = document.getElementById("timer");
+
+const startButtonEl= document.getElementById("start");
+
+const stopButtonEl= document.getElementById("stop");
+
+const resetButtonEl= document.getElementById("reset");
+
+function startTimer(){
+    
+}
