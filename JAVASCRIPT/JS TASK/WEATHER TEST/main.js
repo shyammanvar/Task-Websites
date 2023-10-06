3cdc0b2e481484e7423c92bdc9744669f1b3607b
Mr. Shyam Manvar <142787012+shyammanvar@users.noreply.github.com>
2023-10-06 16:07:23 +0530

    Add files via upload

---
+++ JAVASCRIPT/JS TASK/WEATHER TEST/main.js
@@ -0,0 +1,116 @@
+// const apikey = "46f80a02ecae410460d59960ded6e1c6";
+
+// const weatherDataEl = document.getElementById("weather-data");
+
+// const cityInputEl = document.getElementById("city-input");
+
+// const formEl = document.querySelector("form");
+
+// formEl.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     const cityValue = cityInputEl.value;
+//     getWeatherData(cityValue);
+//   });
+// async function getWeatherData(cityValue){
+//     try {
+//         const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=matric`);
+
+//         if(!response.ok){
+//             throw new Error("Network response was not ok");
+//         }
+// const data = await response.json();
+
+// const temperature = Math.round(data.main.temp);
+
+// const description = data.weather[0].description;
+
+// const icon = data.weather[0].icon;
+
+// const details = [
+//     `Feels like: ${Math.round(data.main.Feels_like)}`,
+//     `Humidity: ${data.main.humidity}%`,
+//     `wind speed: ${data.wind.speed}m/s`,
+// ]
+
+// weatherDataEl.querySelector(".icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="WetherIcon"></img>`;
+// weatherDataEl.querySelector(".temperature").textContent = `${temperature}°C`;
+// weatherDataEl.querySelector(".description").textContent = description;
+// // weatherDataEl.querySelector(".details").innerHTML = details
+// // .map((detail)=> `<div>${detail}</div>`)
+// // .join("");
+// weatherDataEl.querySelector(".details").innerHTML = details
+//       .map((detail) => `<div>${detail}</div>`)
+//       .join("");
+
+//     } catch (error) {
+//         weatherDataEl.querySelector(".icon").innerHTML = "";
+//         weatherDataEl.querySelector(".temperature").textContent = "";
+//         weatherDataEl.querySelector(".description").textContent = "An error happend please try again Later";
+//         weatherDataEl.querySelector(".details").innerHTML = "";
+        
+//     }
+
+// }
+
+
+
+// Refrences
+
+const apikey = "46f80a02ecae410460d59960ded6e1c6";
+
+const weatherDataEl = document.getElementById("weather-data");
+
+const cityInputEl = document.getElementById("city-input");
+
+const formEl = document.querySelector("form");
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const cityValue = cityInputEl.value;
+  getWeatherData(cityValue);
+});
+
+async function getWeatherData(cityValue) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=metric`
+    );
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+
+    const temperature = Math.round(data.main.temp);
+
+    const description = data.weather[0].description;
+
+    const icon = data.weather[0].icon;
+
+    const details = [
+      `Feels like: ${Math.round(data.main.feels_like)}`,
+      `Humidity: ${data.main.humidity}%`,
+      `Wind speed: ${data.wind.speed} m/s`,
+    ];
+
+    weatherDataEl.querySelector(
+      ".icon"
+    ).innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon">`;
+    weatherDataEl.querySelector(
+      ".temperature"
+    ).textContent = `${temperature}°C`;
+    weatherDataEl.querySelector(".description").textContent = description;
+
+    weatherDataEl.querySelector(".details").innerHTML = details
+      .map((detail) => `<div>${detail}</div>`)
+      .join("");
+  } catch (error) {
+    weatherDataEl.querySelector(".icon").innerHTML = "";
+    weatherDataEl.querySelector(".temperature").textContent = "";
+    weatherDataEl.querySelector(".description").textContent =
+      "An error happened, please try again later";
+
+    weatherDataEl.querySelector(".details").innerHTML = "";
+  }
+}
